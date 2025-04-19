a94d2f2eadc1df6a95e508a68ecba9468600fd64
Noreasakpheak018 <rothnoreasakpheak@gmail.com>
2025-04-19 15:43:52 +0700

    modify some methods

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 import hashlib
-# from database import Database
+from database import Database
 
 class AuthSystem:
     def __init__(self, db):
@@ -10,8 +10,7 @@ class AuthSystem:
     def __hash_password(self, password):
         return hashlib.sha256(password.encode()).hexdigest()
 
-
-    # methods for mysql
+    # current user
     def __set_current_user(self, user):
         user_info = {"id": user[0],
                      "username": user[1],
@@ -22,43 +21,61 @@ class AuthSystem:
     def __get_current_user(self):
         return self.current_user
 
+    # methods for mysql/sqlite
     def __select_all_from_db(self, type_):
-        with self.db.conn.cursor() as cursor:
+        cursor = self.db.conn.cursor()
+        result = ""
+        try:
             cursor.execute(f"SELECT {type_} FROM users;")
-            return cursor.fetchall()
+            result = cursor.fetchall()
+        finally:
+            cursor.close()
+        return result
 
     def __get_all_users_from_db(self):
         return self.__select_all_from_db('*')
 
     def __get_user_from_db(self, con_type, con_value):
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(f"SELECT * FROM users WHERE {con_type} = %s;",
+        cursor = self.db.conn.cursor()
+        result = ""
+        try:
+            cursor.execute(f"SELECT * FROM users WHERE {con_type} = ?;",
                            (con_value,))
-            return cursor.fetchone()
+            result = cursor.fetchone()
+        finally:
+            cursor.close()
+
+        return result
 
     def __update_user_in_db(self, con_type, con_val, key, key_val):
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(f"UPDATE users SET {con_type} = %s WHERE {key} = %s;",
+        cursor = self.db.conn.cursor()
+        try:
+            cursor.execute(f"UPDATE users SET {con_type} = ? WHERE {key} = ?;",
                            (con_val, key_val))
             self.db.conn.commit()
+        finally:
+            cursor.close()
 
     def __insert_user_to_db(self, username, password, role):
-
-        with self.db.conn.cursor() as cursor:
-            cursor.execute("""
-                                INSERT IGNORE INTO users (username, password_hash, role)
-                                    VALUES(%s, %s, %s)
-                                """, (username, password, role))
+        cursor = self.db.conn.cursor()
+        try:
+            cursor.execute("""INSERT OR IGNORE INTO users (username, password_hash, role)
+                                    VALUES(?, ?, ?)""", (username, password, role))
             self.db.conn.commit()
+        finally:
+            cursor.close()
 
     def __remove_user_from_db(self, username):
-        with self.db.conn.cursor() as cursor:
-            cursor.execute("DELETE FROM users WHERE username = %s;", (username,))
+        cursor = self.db.conn.cursor()
+        try:
+            cursor.execute("DELETE FROM users WHERE username = ?;", (username,))
             self.db.conn.commit()
+        finally:
+            cursor.close()
 
 
     # methods for auth and personal info
-    def registration(self, username, password, role):
+    def register(self, username, password, role = 'regular'):
         hash_password = self.__hash_password(password)
         role = role if role in ["admin", "regular"] else "regular"
 
@@ -77,16 +94,15 @@ class AuthSystem:
         if user and user[2] == self.__hash_password(password):
             self.__set_current_user(user)
             is_success = True
-
         return is_success
 
     def logout(self):
         self.current_user = None
         return True
 
-    def forget_password(self, id, username, role, new_password):
+    def forget_password(self, id_, username, role, new_password):
         user = self.__get_user_from_db("username", username)
-        if id == user[0] and role == user[3]:
+        if id_ == user[0] and role == user[3]:
             new_password = self.__hash_password(new_password)
             self.__update_user_in_db("username", username,
                                      "password_hash", new_password)
@@ -110,8 +126,7 @@ class AuthSystem:
 
     # admin only methods
     def update_role(self, username, role, admin_password):
-
-        if self.current_user and self.current_user['role'] == 'admin':
+        if self.is_admin():
             user = self.__get_user_from_db("username", username)
             if user and admin_password == self.current_user["password"]:
                 role = role if role in ["admin", "regular"] else "regular"
@@ -121,14 +136,18 @@ class AuthSystem:
         return False
 
     def delete_user(self, username, admin_password):
-        if self.current_user and self.current_user['role'] == 'admin':
+        if self.is_admin():
             user = self.__get_user_from_db("username", username)
             if user and admin_password == self.current_user["password"]:
                 self.__remove_user_from_db(username)
 
-    # testing
-    def __print_users(self):
-        for i in self.__get_all_users_from_db():
-            print(i)
-        print(self.__get_user_from_db("username", "james"))
+    def is_admin(self):
+        """Check if current user is an admin"""
+        return self.current_user and self.current_user['role'] == 'admin'
 
+    # testing methods, delete after dev
+    def print_users(self):
+        for i in self.__get_all_users_from_db():
+            for j in i:
+                print(j, end=" ")
+            print()
